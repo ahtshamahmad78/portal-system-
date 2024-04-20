2e8dac9ddedad2ae4470a7502933216eab0c0165
ahtshamahmad78 <ahtshamahmad875@gmail.com>
2024-04-20 17:25:38 +0500

    first commit

---
+++ shanuMVCUserRoles/Controllers/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using shanuMVCUserRoles.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace shanuMVCUserRoles.Controllers
+{
+	[Authorize]
+	public class UsersController : Controller
+    {
+        ApplicationDbContext context;
+
+        public UsersController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Users
+        public Boolean isAdminUser()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				var user = User.Identity;
+				ApplicationDbContext context = new ApplicationDbContext();
+				var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+				var s = UserManager.GetRoles(user.GetUserId());
+				if (s[0].ToString() == "Admin")
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return false;
+		}
+		public ActionResult Index()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				var user = User.Identity;
+				ViewBag.Name = user.Name;
+				//	ApplicationDbContext context = new ApplicationDbContext();
+				//	var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+				//var s=	UserManager.GetRoles(user.GetUserId());
+				ViewBag.displayMenu = "No";
+
+				if (isAdminUser())
+				{
+					ViewBag.displayMenu = "Yes";
+				}
+				return View();
+			}
+			else
+			{
+				ViewBag.Name = "Not Logged IN";
+			}
+
+
+			return View();
+
+
+		}
+
+        public ActionResult GetUsers()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //var Users = context.Users.ToList();
+            var role = context.Roles.SingleOrDefault(m => m.Name == "Admin");
+            var noneAdminUsers = context.Users.Where(o => !o.Roles.Any(r => r.RoleId == role.Id)).ToList();
+            return View(noneAdminUsers);
+		
+
+
+        }
+    }
+}
---
+++ shanuMVCUserRoles/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shanuMVCUserRoles.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
